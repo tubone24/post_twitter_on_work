3e1e478634ff4d8238f3af5ca4ca5c0e61d3a961
tubone24 <tubo.yyyuuu@gmail.com>
2020-09-18 00:56:26 +0900

    :bug: create secret file

--- scripts/createBase64ToFile.nim
+++ scripts/createBase64ToFile.nim
@@ -1,11 +1,12 @@
 import base64, os
 
 block:
-  let str = $os.commandLineParams()[0]
-  let decodedStr = decode(str)
-  let f : File = open("src/post_twitter_on_workpkg/secret.nim" ,FileMode.fmWrite)
-  f.writeLine(decodedStr)
+  let f1 : File = open("base64.txt" ,FileMode.fmRead)
+  let decodedStr = decode(f1.readAll)
+  let f2 : File = open("src/post_twitter_on_workpkg/secret.nim" ,FileMode.fmWrite)
+  f2.writeLine(decodedStr)
   defer:
-    close(f)
+    close(f1)
+    close(f2)
 
 
