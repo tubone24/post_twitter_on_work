a98a71014fba3fc085178d815fab368814b72a0e
tubone24 <tubo.yyyuuu@gmail.com>
2020-09-18 13:17:19 +0900

    fix config

--- src/post_twitter_on_workpkg/config.nim
+++ src/post_twitter_on_workpkg/config.nim
@@ -1,7 +1,7 @@
 import parsecfg, os, secret
 
 type
-  Config* = ref object of RootObj
+  TwitterConfig* = ref object of RootObj
     appKey*: string
     appKeySecret*: string
     accessToken*: string
@@ -9,7 +9,7 @@ type
 
 proc createSettingCfg() {.discardable.} =
   block:
-    let f : File = open("settings.cfg" ,FileMode.fmWrite)
+    let f : File = open(joinPath(getAppDir(),"settings.cfg") ,FileMode.fmWrite)
     f.writeLine("[auth]")
     f.writeLine("appKey=\"\"")
     f.writeLine("appKeySecret=\"\"")
@@ -18,11 +18,15 @@ proc createSettingCfg() {.discardable.} =
     defer:
       close(f)
 
-proc getConfig*():Config =
- if not os.existsFile("settings.cfg"):
+proc getConfig*():TwitterConfig =
+ if not os.existsFile("settings.cfg") and not os.existsFile(joinPath(getAppDir(),"settings.cfg")):
    createSettingCfg()
- let cfg = loadConfig("settings.cfg")
- result = new Config
+ var cfg: Config
+ if os.existsFile("settings.cfg"):
+   cfg = loadConfig("settings.cfg")
+ elif os.existsFile(joinPath(getAppDir(),"settings.cfg")):
+   cfg = loadConfig(joinPath(getAppDir(),"settings.cfg"))
+ result = new TwitterConfig
  if cfg.getSectionValue("auth", "appKey") == "" and cfg.getSectionValue("auth", "appKeySecret") == "":
    result.appKey = getDefaultAppKey()
    result.appKeySecret = getDefaultAppKeySecret()
@@ -32,8 +36,12 @@ proc getConfig*():Config =
  result.accessToken = cfg.getSectionValue("auth", "accessToken")
  result.accessTokenSecret = cfg.getSectionValue("auth", "accessTokenSecret")
 
-proc setConfig*(section: string, key: string, value: string):Config =
-  var cfg = loadConfig("settings.cfg")
+proc setConfig*(section: string, key: string, value: string):TwitterConfig =
+  var cfg: Config
+  if os.existsFile("settings.cfg"):
+    cfg = loadConfig("settings.cfg")
+  elif os.existsFile(joinPath(getAppDir(),"settings.cfg")):
+    cfg = loadConfig(joinPath(getAppDir(),"settings.cfg"))
   cfg.setSectionKey(section, key, value)
   cfg.writeConfig("settings.cfg")
   return getConfig()
